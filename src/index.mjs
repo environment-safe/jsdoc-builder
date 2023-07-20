e9cbf536ebcae1e22f98f78b7b874cc2d48f5819
Abbey Sparrow <abbey@khrome.net>
2023-07-19 20:53:17 -0600

    more impl

--- src/index.mjs
+++ src/index.mjs
@@ -1,38 +1,129 @@
 import * as fs from 'fs';
-import { join } from 'path';
-import { render } from 'jsdoc-api';
+import * as url from 'url';
+import { join, basename } from 'path';
 import { forEachEmission as forEach } from 'async-arrays/async-arrays.mjs';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import { exec } from 'node:child_process';
+import { glob, globSync, globStream, globStreamSync, Glob } from 'glob';
+
+const runJSDocTypes = async (type, pattern, destination)=>{
+    const files = await glob(pattern, { ignore: 'node_modules/**' });
+    let file = null;
+    for(let lcv=0; lcv< files.length; lcv++){
+        file = files[lcv];
+        const command = `./node_modules/.bin/jsdoc -c "${
+            join(__dirname, '..', `${type}-config.json`)
+        }" -d "${
+            join(__dirname, '..', `${destination}`)
+        }" "${file}"`;
+        
+        console.log(command);
+        await new Promise((resolveExec)=>{
+            exec(command, (error, stdout, stderr) => {
+                if(error) return reject(error);
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+                resolveExec();
+                console.log()
+            }); 
+        });
+        await new Promise((resolveExec)=>{
+            fs.rename(
+                join(__dirname, '..', destination, 'types.d.ts'),
+                join(__dirname, '..', destination, basename(file).replace('.mjs', '.d.ts')),
+                (error)=>{
+                    if(error) return reject(error);
+                    resolveExec();
+                }
+            )
+        });
+    }
+}
+
+const runJSDoc = async (type='', pattern, destination)=>{
+    console.log('TYPE', type)
+    switch(type.toLowerCase()){
+        case 'types' : return await runJSDocTypes(type, pattern, destination);
+        case 'docs' : return await runJSDocDocs(type, pattern, destination);
+        default : throw new Error(`Unknown type: ${type}`);
+    }
+}
+
+const runJSDocDocs = async (type, pattern, destination)=>{
+    const files = await glob(pattern, { ignore: 'node_modules/**' });
+    let file = null;
+    for(let lcv=0; lcv< files.length; lcv++){
+        file = files[lcv];
+        const command = `./node_modules/.bin/jsdoc -c "${
+            join(__dirname, '..', `${type}-config.json`)
+        }" -d "${
+            join(__dirname, '..', `${destination}`)
+        }" "${file}"`;
+        
+        console.log(command);
+        await new Promise((resolveExec)=>{
+            exec(command, (error, stdout, stderr) => {
+                if(error) return reject(error);
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+                resolveExec();
+                console.log()
+            }); 
+        });
+        /*await new Promise((resolveExec)=>{
+            fs.rename(
+                join(__dirname, '..', destination, 'types.d.ts'),
+                join(__dirname, '..', destination, basename(file).replace('.mjs', '.d.ts')),
+                (error)=>{
+                    if(error) return reject(error);
+                    resolveExec();
+                }
+            )
+        });*/
+    }
+}
 
 /**
  * A JSON object
  * @typedef { object } JSON
  */
+ 
+const runJSDocWithConfig = async (type, files, destination)=>{
+    const options = {
+        configure: join(__dirname, '..', `${type}-config.json`),
+        files: join(__dirname, '..', files),
+        destination: (
+            destination === 'console'?
+                'console':
+                join(__dirname, '..', destination)
+        )
+    };
+    console.log(options);
+    const code = jsdoc.renderSync(options);
+    console.log('>>>', code, options);
+    return code;
+};
 
-export const buildDocs = async (source, destination)=>{
-    return new Promise((resolve, reject)=>{
-        const writtenFiles = [];
-        fs.readdir(source, (fileErr, results)=>{
-            forEach(results, (file, index, done)=>{
-                // do the thing
-                done();
-            }, ()=>{
-                resolve(writtenFiles);
-            });
-        });
+
+export const buildTypes = (source, destination)=>{
+    return new Promise(async (resolve, reject)=>{
+        try{
+            const result = await runJSDoc('types', source, destination);
+            resolve(result);
+        }catch(ex){
+            reject(ex);
+        }
     });
 };
 
-export const buildTypes = async (source, destination)=>{
-    return new Promise((resolve, reject)=>{
-        const writtenFiles = [];
-        fs.readdir(source, (fileErr, results)=>{
-            forEach(results, (file, index, done)=>{
-                // do the thing
-                done();
-            }, ()=>{
-                resolve(writtenFiles);
-            });
-        });
+export const buildDocs = (source, destination)=>{
+    return new Promise(async (resolve, reject)=>{
+        try{
+            const result = await runJSDoc('docs', source, destination);
+            resolve(result);
+        }catch(ex){
+            reject(ex);
+        }
     });
 };
 
--- test/test.mjs
+++ test/test.mjs
@@ -1,12 +1,60 @@
 /* global describe:false, it:false */
 import { chai } from '@environment-safe/chai';
-import {  } from '../src/index.mjs';
+import { buildDocs, buildTypes } from '../src/index.mjs';
+import fs from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+import { parse } from "@babel/parser";
+import { rimraf } from 'rimraf'
 const should = chai.should();
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const clearDirectory = async (directory)=>{
+    for(const file of await fs.readdir(directory)){
+        try{
+            const filePath = path.join(directory, file);
+            const absPath = path.join(__dirname, '..', directory, file);
+            const stats = await fs.lstat(filePath);
+            if(stats.isDirectory()) await clearDirectory(filePath);
+            await fs.unlink(absPath);
+        }catch(ex){ console.log(ex); }
+    }
+}
 
 describe('module', ()=>{
     describe('performs a simple test suite', ()=>{
         it('loads', async ()=>{
             should.exist({});
+        });
+        
+        it('transforms types', async ()=>{
+            await rimraf('./test/output/');
+            await buildTypes('./test/example/*.mjs', './test/output/');
+            const source = (await fs.readFile('./test/output/sample.d.ts')).toString();
+            const ast = parse(source, {
+                plugins: [
+                    ["typescript", { dts: true }]
+                ]
+            });
+            ast.program.body[0].type.should.equal('TSModuleDeclaration');
+            ast.program.body[1].type.should.equal('TSModuleDeclaration');
+            const module0Declarations = ast.program.body[0].body.body;
+            module0Declarations[0].type.should.equal('TSDeclareFunction');
+            console.log(
+                module0Declarations[0].id.name,
+                module0Declarations[1].id.name,
+                module0Declarations[2].id.name
+            )
+            module0Declarations[0].id.name.should.equal('getX');
+            module0Declarations[1].type.should.equal('TSDeclareFunction');
+            module0Declarations[1].id.name.should.equal('getY');
+            module0Declarations[2].type.should.equal('TSDeclareFunction');
+            module0Declarations[2].id.name.should.equal('fromString');
+        });
+        
+        it('transforms docs', async ()=>{
+            await buildDocs('./test/example/*.mjs', './test/output/');
         });
     });
 });
