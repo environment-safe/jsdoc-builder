096e761af781c8c2c7324400b47d478ccc90ea7f
Abbey Sparrow <abbey@khrome.net>
2023-07-20 18:56:12 -0600

    bin working

--- bin/build.mjs
+++ bin/build.mjs
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
-import { buildDocs, buildTypes, addFiles } from '../src/index.mjs'
+import { buildDocs, buildTypes } from '../src/index.mjs'
 import { join } from 'path';
 let action = process.argv[2];
 if(!action) throw new Error('No action provided!');
 action = action.toLowerCase();
-const source = process.argv[2] || './src';
-const destination = process.argv[3] || (action === 'docs'?'./docs':'./src' );
+const source = process.argv[3] || './src/*.mjs';
+const destination = process.argv[4] || (action === 'docs'?join('.', 'docs'):join('.', 'src') );
 
-switch(action){
-    case 'docs':
-    
-        break;
-    case 'types':
-    
-        break;
-    case 'add-files':
-    
-        break;
-    default : throw new Error(`unknown action: ${action}`);
-}
+(async ()=>{
+    switch(action){
+        case 'docs':
+            //console.log(`Building files in ${source}`);
+            await buildDocs(source, destination);
+            //console.log(`Built files into ${destination}`);
+            break;
+        case 'types':
+            //console.log(`Building files in ${source}`);
+            await buildTypes(source, destination);
+            //console.log(`Built files into ${destination}`);
+            break;
+        case 'add-files':
+        
+            break;
+        default : throw new Error(`unknown action: ${action}`);
+    }
+})();
---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Build type files as described in the source directory glob into the destination directory
+ * @param source - A glob pattern which describes the input files.
+ * @param destination - The directory to write output files into.
+ */
+declare function buildTypes(source: string, destination: string): void;
+
+/**
+ * Build type files as described in the source directory glob into the destination directory
+ * @param source - A glob pattern which describes the input files.
+ * @param destination - The directory to write output files into.
+ */
+declare function buildDocs(source: string, destination: string): void;
+
